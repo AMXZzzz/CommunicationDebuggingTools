56ee4a8535baa7836858498f493665b9e45bd6ef
E.T. <96624481+AMXZzzz@users.noreply.github.com>
2026-08-09 05:30:27 +0800

    1

---
+++ CommunicationDebuggingTools/Views/Pages/Log/LogPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace CommunicationDebuggingTools.Views.Pages.Log {
+    public partial class LogPage : Page {
+        public LogPage () {
+            InitializeComponent();
+        }
+    }
+}
